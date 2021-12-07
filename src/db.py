88e8ddba58bccfcfca690bb8715bdd42e4492a80
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-12-07 21:13:03 +0200

    Updated database url

--- src/db.py
+++ src/db.py
@@ -3,5 +3,5 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(os.getenv('DB_CONNECTION_STRING'))
+engine = create_engine(os.getenv('DATABASE_URL'))
 Session = sessionmaker(engine)
