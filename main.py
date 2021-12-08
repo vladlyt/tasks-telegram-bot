0d9a35527bfa0e34ed6cf38579d7524b4e4c9adf
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-12-08 11:57:33 +0200

    Added database creation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from src.run import run
+from src.create_db import create_db
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,5 +17,5 @@ PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.getenv('TOKEN')
 
 if __name__ == '__main__':
-    print("RUNNING", TOKEN, PORT)
+    create_db()
     run(TOKEN, PORT)
--- src/create_db.py
+++ src/create_db.py
@@ -1,19 +1,33 @@
 from sqlalchemy import create_engine
 from src.models import User, Group, Task
+from src.db import DATABASE_URL
 
-if __name__ == '__main__':
-    print("Connecting to the database...")
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/postgres')
+
+def drop_db(engine=None):
+    if engine is None:
+        print("Connecting to the database...")
+        engine = create_engine(DATABASE_URL)
 
     print("Dropping tables...")
     User.metadata.drop_all(engine)
     Group.metadata.drop_all(engine)
     Task.metadata.drop_all(engine)
 
+
+def create_db(engine=None):
+    if engine is None:
+        print("Connecting to the database...")
+        engine = create_engine(DATABASE_URL)
+
     print("Creating tables...")
     User.metadata.create_all(engine)
     Group.metadata.create_all(engine)
     Task.metadata.create_all(engine)
 
-    print("Done")
 
+if __name__ == '__main__':
+    print("Connecting to the database...")
+    engine = create_engine(DATABASE_URL)
+    drop_db(engine)
+    create_db(engine)
+    print("Done")
